bc0bf0aa8243e8f74e0bf1adfe6e1a5de62dd94b
chuf <907701820@qq.com>
2024-04-02 20:26:00 +0800

    python 基础

---
+++ code-13-if.py
@@ -0,0 +1,14 @@
+# if
+if True:
+    print('条件为真')
+else:
+    print("条件不成立")
+print("-----")
+
+number = 10
+if number % 2 == 1:
+    print("number是奇数")
+elif number % 2 == 0:
+    print("number是偶数")
+else:
+    print(f"number 的值是{number}")
