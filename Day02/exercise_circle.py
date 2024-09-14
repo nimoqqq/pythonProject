e6f75e1fd581a3ec30367214cf7693d3aeb4a5b6
chufan <lrnimo@163.com>
2024-09-14 14:35:13 +0800

    feat:02语言元素

---
+++ Day02/exercise_circle.py
@@ -0,0 +1,8 @@
+"""
+输入半径计算圆的周长和面积
+"""
+f = float(input('请输入半径: '))
+perimeter = 2 * 3.14 * f
+area = 3.14 * f * f
+print('周长: %.2f' % perimeter)
+print('面积: %.2f' % area)
---
+++ Day02/variable1.py
@@ -0,0 +1,23 @@
+"""
+使用变量保存数据并进行加减乘除运算
+"""
+a = 123
+b = 12
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+"""
+使用type()检查变量的类型
+"""
+a = 100
+b = 12.345
+c = 1 + 5j
+d = 'hello, world'
+e = True
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
